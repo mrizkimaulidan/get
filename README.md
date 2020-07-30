6d5928c6f1c316d087b65085c48bd480146213ba
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-07-30 15:48:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Getfilez
+
+Tools pribadi untuk download file dari website mengunakan wget berdasarkan looping ID dari konten website tersebut. Biasanya untuk download file yang banyak dan memiliki ID yang auto increment
+
+### Prerequisites
+
+Berikut beberapa hal yang perlu diinstal terlebih dahulu:
+
+- Python 3.5.x
+
+### Installing
+
+Instal module yang dibutuhkan
+
+```
+pip install -r requirements.txt
+```
+
+Jalankan program
+
+```
+python script.py
+```
+
+### Preview
+
+Youtube: https://youtu.be/vFWua5tg0Dw
---
+++ script.py
@@ -0,0 +1,43 @@
+import wget
+import os
+
+
+def ask():
+    menu = int(
+        input('1. Download file\n2. Delete file (by size)\n99. EXIT\n\nYour choose: '))
+    if menu == 1:
+        url = input('URL (with protocol): ')
+        max_number = int(input('Max number 1-999: '))
+        file_extension = input('File extension (.pdf, .html): ')
+        file_name = input('File name: ')
+        file_location = input('File location: ')
+        download_file(url, max_number, file_extension,
+                      file_name, file_location)
+    elif menu == 2:
+        directory = input('Directory: ')
+        file_size = int(input('File size (bytes): '))
+        delete_file(directory, file_size)
+    elif menu == 99:
+        print('program exited')
+        exit
+
+
+def download_file(url, max_number, file_extension, file_name, file_location):
+    os.mkdir(file_location)
+    for i in range(1, max_number + 1):
+        site = url + str(i)
+        wget.download(
+            site, file_location + file_name + '-' + str(i) + file_extension)
+
+    print('\n\nDownloaded ' + str(i))
+
+
+def delete_file(directory, file_size):
+    for i in os.scandir(directory):
+        if os.path.getsize(directory + i.name) > file_size:
+            os.remove(directory + i.name)
+            print('Deleted file more than ' +
+                  str(file_size) + ' bytes: ' + i.name)
+
+
+ask()
